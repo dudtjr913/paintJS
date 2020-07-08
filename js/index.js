04168bf29e0dc931c47c32d5359f6b6fab3821c6
dudtjr913 <dudtjr913@naver.com>
2020-07-08 22:52:21 +0900

    painting
    p

---
+++ js/index.js
@@ -0,0 +1,84 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const color = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const button = document.getElementById("jsFillBtn");
+
+INITIAL_STYLE = "";
+CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.lineWidth = 5;
+ctx.strokeStyle = INITIAL_STYLE;
+ctx.fillStyle = INITIAL_STYLE;
+
+let painting = false;
+let filling = false;
+
+function paintingStop() {
+  painting = false;
+}
+function paintingStart() {
+  painting = true;
+}
+
+function mouseMove(event) {
+  const offSetX = event.offsetX;
+  const offSetY = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(offSetX, offSetY);
+  } else {
+    ctx.lineTo(offSetX, offSetY);
+    ctx.stroke();
+  }
+}
+
+function handleFilling() {
+  if (!filling) {
+    button.innerText = "Paint";
+    filling = true;
+  } else {
+    button.innerText = "Fill";
+    filling = false;
+  }
+}
+
+function handleRange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleColor(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", mouseMove);
+  canvas.addEventListener("mousedown", paintingStart);
+  canvas.addEventListener("mouseup", paintingStop);
+  canvas.addEventListener("mouseout", paintingStop);
+  canvas.addEventListener("click", handleClick);
+}
+
+const arrColor = Array.from(color);
+
+arrColor.forEach((color) => color.addEventListener("click", handleColor));
+
+if (range) {
+  range.addEventListener("input", handleRange);
+}
+
+if (button) {
+  button.addEventListener("click", handleFilling);
+}
